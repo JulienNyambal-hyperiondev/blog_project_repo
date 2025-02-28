7bcd3565eaae5a73a4ee5474bf7c581f5806dfa6
Julien Nyambal <Julien.nyambal@gmail.com>
2025-02-28 22:00:14 +0200

    update files

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +76,8 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# Database documentation https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
